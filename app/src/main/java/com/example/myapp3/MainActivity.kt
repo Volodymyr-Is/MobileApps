2b70d8741819939a7a14ac79d276a912cfcf299f
Volodymyr-Is <isaienkovova05@gmail.com>
2025-04-04 11:49:33 +0300

    HW2 App

--- app/src/main/java/com/example/myapp3/MainActivity.kt
+++ app/src/main/java/com/example/myapp3/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
